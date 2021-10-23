9208051e0acc9408a94696ee3cc953071ba00f4a
vardo159 <bramf0@protonmail.com>
2021-10-23 16:02:24 -0500

    sign up's view done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import SignUp from "./components/SignUp"
+
 
 const App = () => {
     return (
-        <div>
-            XD
-        </div>
+        <Container className="d-flex align-items-center justify-content-center"
+                    style={{minHeight: "100vh"}}
+        >
+            <div className="w-100" style={{maxWidth:"400px"}}>
+                <SignUp />
+            </div>
+            
+        </Container>
+        
     );
 }
 
