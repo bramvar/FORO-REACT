f6e309f8a13f15cbd60678babe587471eaf05682
vardo159 <bramf0@protonmail.com>
2021-11-01 18:42:18 -0500

    foro is consuming data from firebase successfully

--- src/components/Discussion.jsx
+++ src/components/Discussion.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Card, Container } from 'react-bootstrap'
+import Comment from './Comment'
 
-export default function Discussion({subject,body}) {
+export default function Discussion({subject,body,author,comments}) {
     return (
-      
-           
                 <Card className="w-50" style={{marginTop:"50px"}}>
                     <Card.Body>
-                        <h2 className="mb-4">{subject}</h2>
+                        <h3 className="mb-4">{subject}</h3>
                         <p>{body}</p>
+                        <div style={{textAlign:"right"}}>
+                            escrito por {author}
+                        </div>
                     </Card.Body>
+                    <Container className="w-70 d-flex justify-content-end flex-column" >
+                        <h4>Comments</h4>
+                        {comments.map((c)=>(
+                            <Comment reply={c.replyText} author={c.creator}/>
+                        ))}
+                        
+                    </Container >
+                    
                 </Card>
    
             
--- src/components/PostSet.jsx
+++ src/components/PostSet.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import {onSnapshot,collection} from "firebase/firestore"
 import {Form, Button, Card, Container} from 'react-bootstrap'
 import Discussion from './Discussion'
+import db from "../firebase.js"
 
 export default function PostSet() {
+
+    const [discussions, setDiscussions] = useState([])
+    console.log(discussions)
+
+    useEffect(() =>     
+        onSnapshot(collection(db,"discussions"),(snapshot)=>
+            setDiscussions(snapshot.docs.map(doc=>doc.data()))
+        ), [])
+
     return (
         <Container className="d-flex align-items-center justify-content-center flex-column">
             
             <Button className="w-50" type="submit" style={{padding:'10px', marginTop:'20px'}}>+ CREAR NUEVA DISCUCIÓN</Button>
-            <Discussion subject="tatan" body="asfsadfad"/>
+            
+            {discussions.map((dis)=>(
+                <Discussion subject={dis.title} body={dis.godMessage} author={dis.creator} comments={dis.replies}/>
+            ))}
+
+            
             
             
         </Container>
